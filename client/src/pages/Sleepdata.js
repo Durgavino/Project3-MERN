1529ebbf93246fcdaa37d44a31e9d7dda930cfa8
Durgavino <mailtodurga93@gmail.com>
2023-04-18 11:13:29 -0300

    pushing the code

--- client/src/pages/Sleepdata.js
+++ client/src/pages/Sleepdata.js
@@ -1,58 +1,3 @@
-// import React, { useState } from 'react';
-
-
-// const Sleepdata = () => {
-//   const [bedTime, setbedTime] = useState('');
-//   const [wakeUpTime, setwakeUpTime] = useState('');
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-//    // const formData = { bedTime, wakeUpTime };
-
-//     const response = await fetch('http://localhost:3000/sleepdata', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify({
-//         query: `
-//         mutation ($bedTime: Number!, $wakeUpTime: Number!) {
-//           createSleepData(bedTime: $bedtime, wakeUpTime: $wakeUpTime) {
-//             id
-//             bedTime
-//             wakeUpTime
-//           }
-//         }
-//       `,
-//       variables: { bedTime, wakeUpTime },
-//       })
-//     });
-//     const data = await response.json();
-//     console.log(data);
-//      }
-//   return (
-//     <div>
-//     <form onSubmit={handleSubmit}>
-//       <label htmlFor="bedTime">Bedtime:</label>
-//       <input type="time" id="bedTime" name="bedTime" value={bedTime} onChange={(e) => setbedTime(e.target.value)} />
-
-//       <label htmlFor="wakeUpTime">Wake-up time:</label>
-//       <input type="time" id="wakeUpTime" name="wakeUpTime" value={wakeUpTime} onChange={(e) => setwakeUpTime(e.target.value)} />
-
-//       <button type="submit">Save</button>
-//     </form>
-//     </div>
-//   )
-
-
-
-
-// }
-
-
-
-// export default Sleepdata;
-
-
 import { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import {SLEEP_DATA} from "../utils/mutations";
@@ -71,14 +16,13 @@ function SleepbedTime(){
 };
 return (
   <form onSubmit={handleSubmit}>
-    <label>
-      Bedtime:
-      <input type="text" value={bedTime} onChange={(e) => setBedtime(e.target.value)} />
-    </label>
-    <label>
-      Wakeuptime:
-      <input type="text" value={wakeUpTime} onChange={(e) => setWakeuptime(e.target.value)} />
-    </label>
+   
+    <label htmlFor="bedTime">Bedtime:</label>
+      <input type="time" id="bedTime" name="bedTime" value={bedTime} onChange={(e) => setBedtime(e.target.value)} />
+   <label htmlFor="wakeUpTime">Wake-up time:</label>
+      <input type="time" id="wakeUpTime" name="wakeUpTime" value={wakeUpTime} onChange={(e) => setWakeuptime(e.target.value)} />
+
+
     <button type="submit">Submit</button>
   </form>
 );
